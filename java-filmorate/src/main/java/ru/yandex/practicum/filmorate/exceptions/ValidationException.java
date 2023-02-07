3136552c6da9ed785ba36a0f772af5a5196e41e0
boltaevt <boltaevt@gmail.com>
2023-02-07 15:22:15 +0200

    again

--- java-filmorate/src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
+++
@@ -1,9 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-class ValidationException extends Exception {
-
-    public ValidationException (String message) {
-        super(message);
-    }
-
-}
